d2b6ba01677b3fa108b218bb23b8d3e566e70b69
madSobirin <gamingbrn202@gmail.com>
2026-01-11 15:41:36 +0700

    membuat tampilan profile admin dan wali

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -16,15 +16,12 @@ class CheckRole
      */
     public function handle(Request $request, Closure $next, string $role): Response
     {
-        // 1. Cek apakah user sudah login
         if (!Auth::check()) {
             return redirect('/login');
         }
 
-        // 2. Cek apakah role user sesuai dengan yang diminta route
-        // Contoh pemanggilan: checkRole:admin
+
         if (Auth::user()->role !== $role) {
-            // Jika role tidak cocok, lempar error 403 (Forbidden) atau redirect
             abort(403, 'AKSES DITOLAK: Anda bukan ' . ucfirst($role));
         }
 
